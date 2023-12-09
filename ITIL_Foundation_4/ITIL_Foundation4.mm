500c408294302baf1e61331c10712ae4f4fb8b1b
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-12-08 21:01:47 -0500

    add ITIL4 structure

---
+++ ITIL_Foundation_4/ITIL_Foundation4.mm
@@ -0,0 +1,119 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="ITIL Foundation 4" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1702086702924">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="0.769">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="1. Introduction" POSITION="bottom_or_right" ID="ID_1330002347" CREATED="1702086738685" MODIFIED="1702086743580">
+<node TEXT="1.1 IT Service Management in the Modern World" ID="ID_1068089045" CREATED="1702086933594" MODIFIED="1702086944560"/>
+<node TEXT="1.2 About ITIL 4" ID="ID_427916781" CREATED="1702086944895" MODIFIED="1702086950333"/>
+<node TEXT="1.3 The Structure and Benefits of the ITIL 4 Framework" FOLDED="true" ID="ID_672799846" CREATED="1702086951121" MODIFIED="1702086964814">
+<node TEXT="1.3.1 The ITIL SVS" ID="ID_1358866762" CREATED="1702086964821" MODIFIED="1702086971161"/>
+<node TEXT="1.3.2 The Four Dimensions Model" ID="ID_197677299" CREATED="1702086971317" MODIFIED="1702086980594"/>
+</node>
+</node>
+<node TEXT="2. Key Concepts of Service Management" POSITION="bottom_or_right" ID="ID_1521593359" CREATED="1702086744000" MODIFIED="1702086752581">
+<node TEXT="2.1 Value and Value Co-Creation" ID="ID_317212764" CREATED="1702086986015" MODIFIED="1702086995301"/>
+<node TEXT="2.2 Organizations, Service Providers, Service Consumers, and other Stakeholders" ID="ID_1705075401" CREATED="1702086995960" MODIFIED="1702087015171"/>
+<node TEXT="2.3 Products and Services" ID="ID_1395936190" CREATED="1702087015423" MODIFIED="1702087022236"/>
+<node TEXT="2.4 Service Relationships" ID="ID_1952446138" CREATED="1702087022385" MODIFIED="1702087027828"/>
+<node TEXT="2.5 Value: Outcomes, Costs, and Risks" ID="ID_231149279" CREATED="1702087028109" MODIFIED="1702087040085"/>
+</node>
+<node TEXT="3. The Four Dimensions of Service Management" POSITION="bottom_or_right" ID="ID_939745173" CREATED="1702086753504" MODIFIED="1702086769112">
+<node TEXT="3.1 Organizations and People" ID="ID_182207074" CREATED="1702087044520" MODIFIED="1702087052944"/>
+<node TEXT="3.2 Information and Technology" ID="ID_1808725529" CREATED="1702087053113" MODIFIED="1702087062141"/>
+<node TEXT="3.3. Partners and Suppliers" ID="ID_550284712" CREATED="1702087062372" MODIFIED="1702087068680"/>
+<node TEXT="3.4 Value Streams and Processes" ID="ID_975440319" CREATED="1702087068831" MODIFIED="1702087075709"/>
+<node TEXT="3.5 External Factors" ID="ID_51553748" CREATED="1702087076321" MODIFIED="1702087081300"/>
+</node>
+<node TEXT="4. The ITIL Service Value Stream" POSITION="bottom_or_right" ID="ID_1773375372" CREATED="1702086772338" MODIFIED="1702086781233">
+<node TEXT="4.1 Service Value System Overview" ID="ID_1001021866" CREATED="1702087086460" MODIFIED="1702087101615"/>
+<node TEXT="4.2 Opportunity, Demand, and Value" ID="ID_1834523253" CREATED="1702087094583" MODIFIED="1702087110603"/>
+<node TEXT="4.3 The ITIL Guiding Principles" ID="ID_1594706174" CREATED="1702087110797" MODIFIED="1702087120225"/>
+<node TEXT="4.4. Governance" ID="ID_1169261606" CREATED="1702087120644" MODIFIED="1702087124012"/>
+<node TEXT="4.5 Service Value Chain" ID="ID_678535057" CREATED="1702087124175" MODIFIED="1702087129723"/>
+<node TEXT="4.6 Continual Imporvement" ID="ID_1121720860" CREATED="1702087133180" MODIFIED="1702087140357"/>
+<node TEXT="4.7 Practices" ID="ID_1006326643" CREATED="1702087140547" MODIFIED="1702087147083"/>
+</node>
+<node TEXT="5. ITIL Management Practices" POSITION="bottom_or_right" ID="ID_1596261799" CREATED="1702086781568" MODIFIED="1702086787308">
+<node TEXT="5.1 General Management Practices" ID="ID_1908488881" CREATED="1702087163863" MODIFIED="1702087175418"/>
+<node TEXT="5.2 Service Management Practices" ID="ID_996832128" CREATED="1702087175599" MODIFIED="1702087183258"/>
+<node TEXT="5.3 Technical Management Practices" ID="ID_1293223738" CREATED="1702087187332" MODIFIED="1702087193811"/>
+</node>
+</node>
+</map>
---
+++ ITIL_Foundation_4/README.md
@@ -0,0 +1,5 @@
+# Learn ITIL Foundation version 4
+
+The source book has following structure:
+
+![ITIL4](img/ITIL_Foundation4.jpg)
